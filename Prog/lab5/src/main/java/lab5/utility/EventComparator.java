67d0a04c7f79619293541ee7822a598ae8a9747b
AndrewGrishchenko <andrew26080537@gmail.com>
2024-02-22 12:20:17 +0300

    sync

--- Prog/lab5/src/main/java/lab5/utility/EventComparator.java
+++ Prog/lab5/src/main/java/lab5/utility/EventComparator.java
@@ -10,6 +10,9 @@ import lab5.models.Event;
 public class EventComparator implements Comparator<Event> {
     @Override
     public int compare(Event o1, Event o2) {
+        if (o1.getTicketsCount() == null && o2.getTicketsCount() == null) return 0;
+        if (o1.getTicketsCount() == null) return -1;
+        if (o2.getTicketsCount() == null) return 1;
         return (int) (o1.getTicketsCount() - o2.getTicketsCount());
     }
 }
