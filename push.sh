7bd2898d70a23fb6f4c45882e820cfc46edd0349
AndrewGrishchenko <andrew26080537@gmail.com>
2024-03-12 01:24:38 +0300

    minor changes

--- push.sh
+++ push.sh
@@ -1,9 +1,9 @@
 #!/bin/zsh
-git add .
 if [ -z "$1" ]
   then
     echo "no argument specified"
     exit
 fi
+git add .
 git commit -m "$*"
 git push "https://AndrewGrishchenko:${GIT_TOKEN}@github.com/AndrewGrishchenko/itmo_labs.git"
