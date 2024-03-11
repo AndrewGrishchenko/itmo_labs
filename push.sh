549a708a57ce49b6e2be3e4925427bee8c2700b8
AndrewGrishchenko <andrew26080537@gmail.com>
2024-03-12 01:23:37 +0300

    added push script

--- push.sh
+++ push.sh
@@ -6,4 +6,4 @@ if [ -z "$1" ]
     exit
 fi
 git commit -m "$*"
-git push "https://AndrewGrishchenko:${GIT_TOKEN}@github.com/AndrewGrishchenko/test.git"
+git push "https://AndrewGrishchenko:${GIT_TOKEN}@github.com/AndrewGrishchenko/itmo_labs.git"
