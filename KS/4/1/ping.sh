eceac954512556c279bff2e08e5ce59d6564baf2
AndrewGrishchenko <andrew26080537@gmail.com>
2026-08-08 23:45:01 +1000

    KS

---
+++ KS/4/1/ping.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ping -s 100 -c 4 gav-it.com
+ping -s 200 -c 4 gav-it.com
+ping -s 500 -c 4 gav-it.com
+ping -s 1000 -c 4 gav-it.com
+ping -s 2000 -c 4 gav-it.com
+ping -s 5000 -c 4 gav-it.com
+ping -s 10000 -c 4 gav-it.com
---
+++ KS/4/2/traceroute.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+traceroute -n gav-it.com
